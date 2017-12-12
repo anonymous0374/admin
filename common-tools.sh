a1d366d24e4ffd8bc910aa5829c2fbf3ffe742bb
j-sparrow <377738226@qq.com>
2017-12-12 12:02:25 +0800

    Update common-tools.sh

--- common-tools.sh
+++ common-tools.sh
@@ -1,1 +1,15 @@
+# install common tools
 sudo apt update && sudo apt upgrade
+sudo apt install chromium-browser
+sudo apt install vim
+
+# install tools from packages
+
+# setup shadowsocksr
+
+# setup .bashrc
+
+# setup shared directory
+
+
+
