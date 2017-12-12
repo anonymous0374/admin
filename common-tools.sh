e7b45ceb2e56fe3f3439a1d2c6746db28f5155ac
j-sparrow <377738226@qq.com>
2017-12-12 13:55:14 +0800

    Update common-tools.sh

--- common-tools.sh
+++ common-tools.sh
@@ -12,6 +12,13 @@ sudo apt install glipper
 sudo apt install libsodium-dev
 sudo apt-get install ibus-sunpinyin sunpinyin-data
 
+# install mongodb server
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
 # install tools from packages
 
 # setup shadowsocksr
